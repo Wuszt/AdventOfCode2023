cf0b5a6ed9393cb09f2f284faeeff944ace56015
wuszt-laptop <igoh4ni@gmail.com>
2023-12-04 21:09:32 +0100

    Minor improvements

--- AOC_4/AOC_4.py
+++ AOC_4/AOC_4.py
@@ -1,13 +1,7 @@
 lines = open("input.txt", "r").readlines()
-crds = [1] * len(lines)
-sums = [0,0]
-for i, nr in enumerate(nr.split('|') for nr in [x[x.index(':')+1:].rstrip() for x in lines]):
-    winners = nr[0].strip().split(' ')
-    my = [x for x in nr[1].strip().split(' ') if x != '']
-    offset = 1
-    for m in my:
-        if m in winners:
-          crds[i + offset] += 1 * crds[i]
-          offset += 1
-    sums = [sums[0] + (pow(2,offset - 2) if offset > 1 else 0), sums[1] + crds[i]]
+crds, sums = [1] * len(lines), [0,0]
+for i, nr in enumerate([x[x.index(':')+1:].rstrip().split('|') for x in lines]):
+    my = [x for x in nr[1].strip().split(' ') if x != '' and x in nr[0].strip().split(' ')]
+    for o in range(len(my)): crds[i + o + 1] += crds[i]
+    sums = [sums[0] + int(pow(2,len(my) - 1)), sums[1] + crds[i]]
 print(sums)
